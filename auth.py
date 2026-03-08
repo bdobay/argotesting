a94d6e9e96e8a781e9317ea4d0c0c6417d02cf7c
Blake Dobay <44700437+bdobay@users.noreply.github.com>
2026-03-09 09:43:25 +1000

    Update auth.py

--- auth.py
+++ auth.py
@@ -1,8 +1,7 @@
 import google.auth
 from google.auth.transport.requests import Request
 
-credentials, project = google.auth.default()
-
+credentials, project = google.auth.default(scopes=["https://www.googleapis.com/auth/cloud-platform"])
 credentials.refresh(Request())
 
 print("Project:", project)
